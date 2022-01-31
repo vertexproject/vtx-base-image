3db44afb182e5d2fb1a1e6f22f34e6cd8c127440
epiphyte <epiphyte@vertex.link>
2022-01-31 15:56:05 -0500

    Test python executable paths

--- test/synapse_test.sh
+++ test/synapse_test.sh
@@ -3,5 +3,8 @@ apt update
 apt install -y git
 git clone https://github.com/vertexproject/synapse.git -v --depth 1 ./synapse
 cd ./synapse
-pytest -v -s --disable-warnings --durations 6 --maxfail 6
+# pytest -v -s --disable-warnings --durations 6 --maxfail 6
+which python3
+which python
+# python3 -m pytest -v -s --disable-warnings --durations 6 --maxfail 6
 exit $?
