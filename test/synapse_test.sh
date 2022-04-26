258ce3ef6a56cce695ed605aaddf55feb92e83f2
epiphyte <epiphyte@vertex.link>
2022-04-25 21:31:41 -0400

    update test harness

--- test/synapse_test.sh
+++ test/synapse_test.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env bash
-apt update
-apt install -y git
+#!/usr/bin/env sh
+apk update
+apk add git
 git clone https://github.com/vertexproject/synapse.git -v --depth 1 ./synapse
 cd ./synapse
-which python3
-which python
 SYN_VENDOR_TEST=1 python3 -m pytest -v -s --disable-warnings --durations 6 --maxfail 6
 # pytest -v -s --disable-warnings --durations 6 --maxfail 6
 exit $?
