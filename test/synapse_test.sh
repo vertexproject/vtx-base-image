d1df13edb26a8c8525c2d04dcf18aa78f8cddd61
epiphyte <epiphyte@vertex.link>
2023-08-08 14:36:50 +0000

    inline the whole command

--- test/synapse_test.sh
+++ test/synapse_test.sh
@@ -5,7 +5,6 @@ git clone https://github.com/vertexproject/synapse.git -v --depth 1 ./synapse
 cd ./synapse
 which python3
 which python
-PYTEST_ARGS='-v -s --disable-warnings --durations 6 --maxfail 1 -k "not test_slab_mapfull_runsyncloop"'
-SYN_VENDOR_TEST=1 python3 -m pytest $PYTEST_ARGS
+SYN_VENDOR_TEST=1 python3 -m pytest -v -s --disable-warnings --durations 6 --maxfail 1 -k "not test_slab_mapfull_runsyncloop"
 # pytest -v -s --disable-warnings --durations 6 --maxfail 6
 exit $?
