cf9b27ba2a6e1f23857af8d240c511b7c33c80ce
epiphyte <epiphyte@vertex.link>
2023-08-08 14:08:42 +0000

    Maxfail=1, skip flaky test (SYN-5947)

--- test/synapse_test.sh
+++ test/synapse_test.sh
@@ -5,6 +5,7 @@ git clone https://github.com/vertexproject/synapse.git -v --depth 1 ./synapse
 cd ./synapse
 which python3
 which python
-SYN_VENDOR_TEST=1 python3 -m pytest -v -s --disable-warnings --durations 6 --maxfail 6
+PYTEST_ARGS='-v -s --disable-warnings --durations 6 --maxfail 1 -k "not test_slab_mapfull_runsyncloop"'
+SYN_VENDOR_TEST=1 python3 -m pytest $PYTEST_ARGS
 # pytest -v -s --disable-warnings --durations 6 --maxfail 6
 exit $?
