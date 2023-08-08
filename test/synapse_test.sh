a06fbcb5875c7b1badfded891f39f57cefbd2c6a
epiphyte <epiphyte@vertex.link>
2023-08-08 21:02:40 +0000

    pytest.ini, skip a few problematic tests

--- test/synapse_test.sh
+++ test/synapse_test.sh
@@ -3,8 +3,9 @@ apt update
 apt install -y git
 git clone https://github.com/vertexproject/synapse.git -v --depth 1 ./synapse
 cd ./synapse
+cp -v /opt/test/pytest.ini .
 which python3
 which python
-SYN_VENDOR_TEST=1 python3 -m pytest -v -s --disable-warnings --durations 6 --maxfail 1  --log-cli-level=DEBUG --log-cli-format="%(asctime)s [%(levelname)s] %(message)s [%(filename)s:%(funcName)s:%(threadName)s:%(processName)s]" --log-cli-date-format="%Y-%m-%d %H:%M:%S" -k "not test_slab_mapfull_runsyncloop"
+SYN_VENDOR_TEST=1 python3 -m pytest
 # pytest -v -s --disable-warnings --durations 6 --maxfail 6
 exit $?
