503b4bec947ea639999bad586c4ac6b0c50260b7
epiphyte <epiphyte@vertex.link>
2023-08-08 15:14:59 +0000

    Add logging to test execution

--- test/synapse_test.sh
+++ test/synapse_test.sh
@@ -5,6 +5,6 @@ git clone https://github.com/vertexproject/synapse.git -v --depth 1 ./synapse
 cd ./synapse
 which python3
 which python
-SYN_VENDOR_TEST=1 python3 -m pytest -v -s --disable-warnings --durations 6 --maxfail 1 -k "not test_slab_mapfull_runsyncloop"
+SYN_VENDOR_TEST=1 python3 -m pytest -v -s --disable-warnings --durations 6 --maxfail 1  --log-cli-level=DEBUG --log-cli-format="%(asctime)s [%(levelname)s] %(message)s [%(filename)s:%(funcName)s:%(threadName)s:%(processName)s]" --log-cli-date-format="%Y-%m-%d %H:%M:%S" -k "not test_slab_mapfull_runsyncloop"
 # pytest -v -s --disable-warnings --durations 6 --maxfail 6
 exit $?
